237eb1fc1bd10f33265406b8b8b21d6e41c2c03e
Viktor Gerasimenko <viktor.gerasimenko.2014@mail.ru>
2020-09-27 13:11:36 +0300

    Add files via upload

--- filters.cpp
+++ filters.cpp
@@ -15,8 +15,6 @@ int Filter::GetMedianValueInBox(int xCentre, int yCentre, int radius, image_data
 
 	for (auto y = yCentre - radius; y <= yCentre + radius; y++) {
 		for (auto x = xCentre - radius; x <= xCentre + radius; x++) {
-			if (x == xCentre && y == yCentre)
-				continue;
 			if (IsInActiveArea(x, y)) {
 				unsigned char* p = pictureData.pixels + y * pictureData.w * pictureData.compPerPixel
 					+ x * pictureData.compPerPixel;
@@ -24,7 +22,7 @@ int Filter::GetMedianValueInBox(int xCentre, int yCentre, int radius, image_data
 			}
 		}
 	}
-
+	std::sort(res.begin(), res.end());
 	return res[res.size() / 2];
 
 }
