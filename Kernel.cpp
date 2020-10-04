d254fe0bab8fddc72ae9a217b88767304c674350
Viktor Gerasimenko <viktor.gerasimenko.2014@mail.ru>
2020-10-04 22:30:46 +0300

    Add files via upload

--- Kernel.cpp
+++ Kernel.cpp
@@ -3,27 +3,25 @@
 
 Kernel::Kernel(matrix setWeights) {
 	weights = setWeights;
-	/*sumWeights = 0;
+	sumWeights = 0;
 	for (auto i = 0; i < weights.size(); i++) {
 		for (auto j = 0; j < weights[i].size(); j++) {
 			sumWeights += weights[i][j];
 		}
-	}*/
+	}
 	radius = 1;
 }
 
 
 stbi_uc Kernel::Apply(int xCentre, int yCentre, int color, active_rectangle& activeArea, image_data& pictureData) {
-	int res = 0;
-	int sumWeights = 0;
+	double res = 0;
 	for (auto y = yCentre - radius, yMatrix = 0; y <= yCentre + radius; y++, yMatrix++) {
 		for (auto x = xCentre - radius, xMatrix = 0; x <= xCentre + radius; x++, xMatrix++) {
-			if (x >= activeArea.leftColumn && x <= activeArea.rightColumn &&
-				y >= activeArea.upperLine && y <= activeArea.bottomLine) {
+			if (x >= activeArea.leftColumn && x < activeArea.rightColumn &&
+				y >= activeArea.upperLine && y < activeArea.bottomLine) {
 				unsigned char* p = pictureData.pixels + y * pictureData.w * pictureData.compPerPixel
 					+ x * pictureData.compPerPixel;
 				res += weights[yMatrix][xMatrix] * p[color];
-				sumWeights += weights[yMatrix][xMatrix];
 			}
 		}
 	} 
